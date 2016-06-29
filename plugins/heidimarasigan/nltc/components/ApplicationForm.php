8d9dd826e6b22cce4e4231645913a6159ec9517b
Gideon Mark Pacete <gideonmarkcpacete@gmail.com>
2016-06-29 19:30:23 +0800

    Changes in Saving 2

--- plugins/heidimarasigan/nltc/components/ApplicationForm.php
+++ plugins/heidimarasigan/nltc/components/ApplicationForm.php
@@ -811,6 +811,7 @@ class ApplicationForm extends ComponentBase
         if($application->validate())
         {
             Session::put('interview_details', $post);
+            
             if( $post['setinterview'] == 'true' )
                 $this->saveData();
         }
